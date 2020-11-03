8e7cadceecdd28cee94eed0db1a2cc86c5eb22ac
MorReky <mormemir@yandex.ru>
2020-11-03 05:02:57 +0000

    Тестирование модели-представления

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pattern_MVVM.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace Pattern_MVVM.ViewModels
 {
-    class MainWindowViewModel
+   internal class MainWindowViewModel:ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики ПО";
+        /// <summary> Заголовок окна </summary>
+        public string Title 
+        {
+            get => _Title;
+            //Или еще проще
+            //set
+            //{
+            //    //Т.К. есть ранее описнное свойство Set, то можем сократить код
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
