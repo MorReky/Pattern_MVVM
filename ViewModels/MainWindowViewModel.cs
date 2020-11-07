1d78612ae358088e0ca6c96d6a0cdda971023c67
MorReky <mormemir@yandex.ru>
2020-11-07 08:32:41 +0000

    Добавляем в модель-представления Главного окна комманду выхода из приложения

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,21 @@
-﻿using Pattern_MVVM.ViewModels.Base;
+﻿using Pattern_MVVM.Infrastructure.Commands;
+using Pattern_MVVM.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Pattern_MVVM.ViewModels
 {
-   internal class MainWindowViewModel:ViewModel
+    internal class MainWindowViewModel : ViewModel
     {
         #region Заголовок окна
         private string _Title = "Анализ статистики ПО";
         /// <summary> Заголовок окна </summary>
-        public string Title 
+        public string Title
         {
             get => _Title;
             //Или еще проще
@@ -28,9 +31,8 @@ namespace Pattern_MVVM.ViewModels
         }
         #endregion
 
-        #region
-        /// <summary> Статус программы </summary>
-        private string _Status="Готов!";
+        #region Статус программы 
+        private string _Status = "Готов!";
         /// <summary> Статус программы </summary>
         public string Status
         {
@@ -38,5 +40,27 @@ namespace Pattern_MVVM.ViewModels
             set => Set(ref _Status, value);
         }
         #endregion
+
+        #region Комманды
+        #region CloseApplicationCommand
+        //В этом свойстве находится сама комманда, а методы лишь оперделяют её
+        public ICommand CloseApplicationCommand { get; }
+        //Выполняется в момент выполнения комманды
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Комманды
+            //запихиваем в свойство комманду
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted,CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
